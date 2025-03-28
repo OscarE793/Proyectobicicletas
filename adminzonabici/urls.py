8e45150806a3678bee2af547ad804f2c08513294
Oscar Jaramillo <oejaramillop@agarez.net>
2025-03-27 23:53:08 -0500

    Primer commit: configuración inicial del proyecto

---
+++ adminzonabici/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.shortcuts import redirect
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', lambda request: redirect('admin/')),
+]
